279befb2deadee349484ab23371e03d543a39e84
Mark Eaton <mark@LAdmins-MacBook-Pro.local>
2019-02-06 13:46:07 -0600

    updated response

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use( bodyParser.json() );
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/helloworld', (req, res) => {
   console.log(req.body.data);
-  res.status(200).send('Received Data Successfully!')
+  res.status(200).send(req.body.data)
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
